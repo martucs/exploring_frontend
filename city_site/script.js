2dfa10c01e331f70ae832fa6b16bc84b2b58a8b1
martucs <martalopezcab@gmail.com>
2025-11-21 18:35:31 +0100

    added buttons, list, form + start js

---
+++ city_site/script.js
@@ -0,0 +1,8 @@
+/* esperamos a que la pagina este cargada */
+document.addEventListener("DOMContentLoaded", function(){
+
+    let button = document.getElementById("mybutton");
+    /* printeamos el elemento en la console de la pagina*/
+    console.log(button);
+   
+});
